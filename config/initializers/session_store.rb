2a78c7c4a7a0c6283e8332b364196f42f1e2c629
Danilo Franca <dlf3@cin.ufpe.br>
2019-11-16 03:05:10 -0300

    session store updated

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_ApiCaelumEAP", domain: "https://api-caelum-eap.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_ApiCaelumEAP", domain: "api-caelum-eap.herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_ApiCaelumEAP"
 end
